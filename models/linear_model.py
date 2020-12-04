123c6be0d7bdfa7402fbb63a4286c8d75a0d13eb
Chase Lee <chase412@cs.washington.edu>
2020-12-03 21:33:49 -0800

    Deleted unnecessary files and changed code to utilize cuda. and etc

--- models/linear_model.py
+++ models/linear_model.py
@@ -7,15 +7,18 @@ from utils.cachedir import cache_dir
 
 import numpy as np
 import pickle
+import pdb
+import tqdm
 
 input_size = 768
 output_size = 1
 
-hidden_size = 20
-batch_size = 4
-num_epochs = 500
-learning_rate = 0.0001
-
+hidden_size = 512
+batch_size = 64
+num_epochs = 5
+learning_rate = 0.001
+WEIGHT_DECAY = 0.0005
+device = torch.device('cuda')
 # TODO this is a basic linear regression model, we can probably get
 # higher accuracy with LSTM
 
@@ -27,24 +30,27 @@ class Net(torch.nn.Module):
         self.fc2 = torch.nn.Linear(hidden_size, output_size)
 
     def forward(self, x):
-        x = F.relu(self.fc1(x))
+        x = F.leaky_relu(self.fc1(x))
         x = self.fc2(x)
         return x
 
 
 class LinearModel():
     def __init__(self, train_data, test_data):
-        self.net = Net()
+        self.net = Net().to(device)
         self.train_data = train_data
         self.test_data = test_data
         self.loss_fn = torch.nn.MSELoss()
-        self.opt = torch.optim.SGD(self.net.parameters(), lr=learning_rate)
+        self.opt = torch.optim.SGD(self.net.parameters(), lr=learning_rate, weight_decay=WEIGHT_DECAY)
 
     def train(self):
         i = 0
-        for _ in range(num_epochs):
-            for batch in self.train_data:
+        for epoch in range(num_epochs):
+            train_loader = torch.utils.data.DataLoader(self.train_data, batch_size=batch_size, shuffle=True)
+            for idx, batch in enumerate(tqdm.tqdm(train_loader, desc=f'Training {epoch+1} epoch')):
                 x, y = self.get_pair(batch)
+                y = y.to(device)
+                x = embedding.embed_batch(x)
                 y_hat = self.net(x)
                 loss = self.loss_fn(y_hat, y)
 
@@ -52,10 +58,9 @@ class LinearModel():
                 loss.backward()
                 self.opt.step()
 
-                if i % (4000 / batch_size) == 0:
+                if idx % (4000 / batch_size) == 0:
                     print(loss.item())
-
-                i += 1
+            self.test()
         print('done training')
 
     def test(self):
@@ -65,27 +70,30 @@ class LinearModel():
         correct = 0
         total = 0
         losses = []
-
+        MAE = []
         # In case of python < 3.5
         def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
             return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
 
         with torch.no_grad():
-            for batch in self.test_data:
+            test_loader = torch.utils.data.DataLoader(self.test_data, batch_size=batch_size, shuffle=False)
+            for idx, batch in enumerate(tqdm.tqdm(test_loader, desc=f'Test ')):
                 x, y = self.get_pair(batch)
+                x = embedding.embed_batch(x)
                 y_hat = self.net(x)
+                y = y.to(device)
                 losses.append(self.loss_fn(y_hat, y))
+                MAE.append(F.l1_loss(y_hat, y))
+                # for b in range(batch_size):
+                #     yb = y[b].item()
+                #     yhb = y_hat[b].item()
+                #     if isclose(yb, yhb, rel_tol=tolerance):
+                #         correct += 1
+                #     total += 1
 
-                for b in range(batch_size):
-                    yb = y[b].item()
-                    yhb = y_hat[b].item()
-                    if isclose(yb, yhb, rel_tol=tolerance):
-                        correct += 1
-                    total += 1
-
-        print(f'Test accuracy: {correct / total}')
-        print(f'Test loss: {np.mean(losses)}')
-
+        # print(f'Test accuracy: {correct / total}')
+        print(f'Average MSE loss for Test: {torch.mean(torch.Tensor(losses))}')
+        print(f"Average MAE loss for Test: {torch.mean(torch.Tensor(MAE))}")
     def score(self, seq):
         tokens = embedding.tokenize(seq)
         embedded_seq = embedding.embed_line(tokens)
@@ -96,18 +104,21 @@ class LinearModel():
     @staticmethod
     def get_pair(batch):
         x, y = batch
-        y = torch.Tensor(y)
+        y = torch.Tensor(y.float())
         y = torch.unsqueeze(y, 1)
-        return Variable(x), Variable(y)
+        return x, y
 
     @staticmethod
     def load_data():
         train, test = embedding.create_dataset('data/train_funlines.csv')
         print('done splitting')
-        train = embedding.embed(train)
-        test = embedding.embed(test)
-        train = embedding.batch_embedded(train, batch_size)
-        test = embedding.batch_embedded(test, batch_size)
+        data_train = embedding.FunLineDataset(train)
+        data_test = embedding.FunLineDataset(test)
+
+        # train = embedding.embed(train)
+        # test = embedding.embed(test)
+        # train = embedding.batch_embedded(train, batch_size)
+        # test = embedding.batch_embedded(test, batch_size)
         print('done embedding')
-        pickle.dump(train, open(f'{cache_dir}/train_embedded.p', 'wb'))
-        pickle.dump(test, open(f'{cache_dir}/test_embedded.p', 'wb'))
+        pickle.dump(data_train, open(f'{cache_dir}/train_embedded.p', 'wb'))
+        pickle.dump(data_test, open(f'{cache_dir}/test_embedded.p', 'wb'))
--- utils/embedding.py
+++ utils/embedding.py
@@ -6,15 +6,31 @@ from sklearn.model_selection import train_test_split
 from transformers import BertTokenizer, BertModel
 import csv
 import re
+import tqdm
+import multiprocessing
 
-
-device = torch.device('cpu')
+device = torch.device('cuda')
 
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertModel.from_pretrained(
     'bert-base-uncased', return_dict=True).to(device)
 
 max_length = 42
+
+class FunLineDataset(torch.utils.data.Dataset):
+    def __init__(self, data):
+        super(FunLineDataset, self).__init__()
+        self.data = data
+
+    def __len__(self):
+        # TODO return the number of unique sequences you have, not the number of characters.
+        return len(self.data)
+        
+    def __getitem__(self, idx):
+        return self.data['Text'][idx], self.data['Mean_Score'][idx]
+
+
+
 
 
 # Creates train and test data in pands dataframe
@@ -24,7 +40,8 @@ def create_dataset(path):
     with open(path, encoding="utf8", errors='ignore') as f:
         rows = csv.reader(f)
         rows = list(rows)
-        for row in rows[1:100]:
+        for row in rows[1:]:
+            if float(row[4]) == 0: continue
             match = re.search(r'<.*>', row[1])
             sentence = row[1][:match.start()] + row[2] + row[1][match.end():]
             data.append((sentence, float(row[4])))
@@ -37,16 +54,15 @@ def create_dataset(path):
 
 
 # Embeds sentences through language model
-def embed(data_set):
-    sentences = list(data_set['Text'])
-    scores = list(data_set['Mean_Score'])
-    embeddings = []
+# def embed(data_set):
+#     sentences = list(data_set['Text'])
+#     scores = list(data_set['Mean_Score'])
+#     embeddings = []
 
-    for idx, sentence in enumerate(sentences):
-        inputs = tokenize(sentence)
-        embeddings.append((embed_line(inputs), scores[idx]))
-
-    return embeddings
+#     for idx, sentence in enumerate(tqdm.tqdm(sentences, desc="Embedding: ")):
+#         inputs = tokenize(sentence)
+#         embeddings.append((embed_line(inputs), scores[idx]))
+#     return embeddings
 
 
 def tokenize(sentence):
@@ -61,39 +77,49 @@ def embed_line(tokens):
     return model(**tokens)
 
 
+def embed_batch(data):
+    sentences = list(data)
+    embeddings = []
+    for idx, sentence in enumerate(sentences):
+        inputs = tokenize(sentence)
+        # Getting [CLS] token
+        embeddings.append(embed_line(inputs)[0][:,0,:])
+    return torch.stack(embeddings).squeeze().to(device)
+
 # Returns batch of (tensor of batch_size x embed size, batch_size x mean_score)
-def batch_embedded(data, size):
-    random.shuffle(data)
-    num_batch = len(data)//size
+# def batch_embedded(data, size):
+#     random.shuffle(data)
+#     num_batch = len(data)//size
 
-    batches = []
-    for i in range(num_batch):
-        batch_features = []
-        batch_scores = []
+#     batches = []
+#     for i in range(num_batch):
+#         batch_features = []
+#         batch_scores = []
 
-        for (states, scores) in data[i*size:(i+1)*size]:
-            # Extract [CLS] embedding
-            batch_features.append(states[0][:, 0, :])
-            batch_scores.append(scores)
+#         for (states, scores) in data[i*size:(i+1)*size]:
+#             # Extract [CLS] embedding
+#             batch_features.append(states[0][:, 0, :])
+#             batch_scores.append(scores)
 
-        batches.append((
-            torch.stack(batch_features).squeeze(),
-            batch_scores
-        ))
+#         batches.append((
+#             torch.stack(batch_features).squeeze(),
+#             batch_scores
+#         ))
 
-    # Putting last batch which is smaller than others
-    if len(data[(i+1)*size:]) > 0:
-        batch_features = []
-        batch_scores = []
+#     # Putting last batch which is smaller than others
+#     if len(data[(i+1)*size:]) > 0:
+#         batch_features = []
+#         batch_scores = []
 
-        for (states, scores) in data[(i+1)*size:]:
-            # Extract [CLS] embedding
-            batch_features.append(states[0][:, 0, :])
-            batch_scores.append(scores)
+#         for (states, scores) in data[(i+1)*size:]:
+#             # Extract [CLS] embedding
+#             batch_features.append(states[0][:, 0, :])
+#             batch_scores.append(scores)
 
-        batches.append((
-            torch.stack(batch_features).squeeze(),
-            batch_scores
-        ))
+#         batches.append((
+#             torch.stack(batch_features).squeeze(),
+#             batch_scores
+#         ))
 
-    return batches
+#     return batches
+
